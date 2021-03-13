e58acc4ddf237a6c49a4067fd20f3df4c99f64d4
yliu15 <45496965+yliu15@users.noreply.github.com>
2021-03-13 11:02:05 -0500

    Create test.sh

---
+++ mapreduce-test-python/part1/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+../../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /part1/input/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-file ../../mapreduce-test-python/part1/mapper.py -mapper ../../mapreduce-test-python/part1/mapper.py \
+-file ../../mapreduce-test-python/part1/reducer.py -reducer ../../mapreduce-test-python/part1/reducer.py \
+-input /part1/input/* -output /part1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /part1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
+../../stop.sh
