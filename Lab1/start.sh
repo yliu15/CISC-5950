e62fdafbd2d93dcd62c6091c46ec1a173d5b95c3
yliu15 <45496965+yliu15@users.noreply.github.com>
2021-03-13 09:01:25 -0500

    Delete Lab1 directory

--- Lab1/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-/usr/local/hadoop/sbin/start-dfs.sh
-/usr/local/hadoop/sbin/start-yarn.sh
-/usr/local/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
-/usr/local/hadoop/bin/hdfs dfsadmin -safemode leave
--- Lab1/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/usr/local/hadoop/sbin/stop-dfs.sh
-/usr/local/hadoop/sbin/stop-yarn.sh
-/usr/local/hadoop/sbin/mr-jobhistory-daemon.sh stop historyserver
